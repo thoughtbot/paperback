a5530b7877b2e1979f602f507f499838e43c0637
Joe Ferris <jferris@thoughtbot.com>
2013-03-29 12:48:43 -0400

    Replace pdf_info.ps with pdf-reader gem

--- features/support/pdf_info.rb
+++ features/support/pdf_info.rb
@@ -1,29 +1,42 @@
+require 'pdf-reader'
+
 module PdfInfo
-  PAGE_SIZE_6_BY_9_INCHES = %w(432 648)
+  PAGE_SIZE_6_BY_9_INCHES = [0, 0, 432, 648]
   PDF_INFO_PATH = File.expand_path('../pdf_info.ps', __FILE__)
   SCREEN_FONTS = %w(ProximaNova Inconsolata)
   SHOW_EMBEDDED_FONTS_FLAG = '-dDumpFontsUsed -dShowEmbeddedFonts'
   SHOW_PAGE_SIZES_FLAG = '-dDumpMediaSizes'
 
   def embedded_fonts(pdf_path)
-    output = pdf_info(pdf_path, SHOW_EMBEDDED_FONTS_FLAG)
-    output.scan(/^[A-Z]+\+(\w+)/).flatten.uniq
+    pdf_info(pdf_path).
+      pages.
+      map { |page| page.fonts.values }.
+      flatten.
+      map { |spec| spec[:BaseFont].to_s.scan(/^[A-Z]+\+(\w+)/).first }.
+      flatten.
+      uniq
   end
 
   def page_sizes(pdf_path)
-    output = pdf_info(pdf_path, SHOW_PAGE_SIZES_FLAG)
-    output.scan(/Page \d+ MediaBox: \[ (\d+) (\d+) \]/).uniq
+    pdf_info(pdf_path).
+      pages.
+      map { |page| page_size(page) }.
+      uniq
   end
 
-  def pdf_info(pdf_path, flags)
+  def page_size(page)
+    objects = page.objects.values
+
+    media_object = objects.detect do |object|
+      object.respond_to?(:to_hash) && object.to_hash[:MediaBox]
+    end
+
+    media_object[:MediaBox]
+  end
+
+  def pdf_info(pdf_path)#, flags)
     in_current_dir do
-      raw_output = `gs \
-        -dNODISPLAY \
-        -q \
-        -sFile=#{pdf_path} \
-        #{flags} \
-        #{PDF_INFO_PATH}`
-      raw_output.encode!('UTF-8', 'UTF-8', invalid: :replace)
+      PDF::Reader.new(pdf_path)
     end
   end
 end
