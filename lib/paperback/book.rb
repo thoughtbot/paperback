39bf88853752b65a8b7f5985d4675ce25db2f235
Trevor O'Reilly <trevor@thoughtbot.com>
2013-06-10 11:43:22 -0400

    Add Pandoc delegates to Book

--- lib/paperback/book.rb
+++ lib/paperback/book.rb
@@ -12,9 +12,9 @@ module Paperback
       to_markdown
 
       Paperback.in_build_dir do
-        @pandoc.to_html
+        to_html
         to_mobi
-        @pandoc.to_pdf
+        to_pdf
         to_toc
       end
     end
@@ -23,7 +23,7 @@ module Paperback
       to_markdown
 
       Paperback.in_build_dir do
-        @pandoc.to_pdf
+        to_pdf
         Cocaine::CommandLine.new('open', @package.target(:pdf)).run
       end
     end
@@ -32,12 +32,20 @@ module Paperback
       to_markdown
 
       Paperback.in_build_dir do
-        @pandoc.to_pdf
+        to_pdf
         Stats.new(@package.target(:pdf))
       end
     end
 
     private
+
+    def to_epub
+      @pandoc.to_epub
+    end
+
+    def to_html
+      @pandoc.to_html
+    end
 
     def to_markdown
       Markdown.new(
@@ -47,8 +55,12 @@ module Paperback
     end
 
     def to_mobi
-      @pandoc.to_epub
+      to_epub
       Kindlegen.run "#{@package.target(:epub)} -o #{@package.target(:mobi)}"
+    end
+
+    def to_pdf
+      @pandoc.to_pdf
     end
 
     def to_toc
