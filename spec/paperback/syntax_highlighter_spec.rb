c7ee54adbbfc7d8e064e14dbe40034fee475a54e
Trevor O'Reilly <trevor@thoughtbot.com>
2013-08-09 10:53:26 -0400

    Support indented code references
    https://www.apptrajectory.com/thoughtbot/learn/stories/15633204

--- spec/paperback/syntax_highlighter_spec.rb
+++ spec/paperback/syntax_highlighter_spec.rb
@@ -9,7 +9,7 @@ describe Paperback::SyntaxHighlighter do
     context 'known file type' do
       it 'includes a code fence with a language' do
         syntax_highlighter = Paperback::SyntaxHighlighter.new(
-          'user.rb', 'abc123', nil
+          '', 'user.rb', 'abc123', nil
         )
 
         actual = syntax_highlighter.to_ary
@@ -21,7 +21,7 @@ describe Paperback::SyntaxHighlighter do
     context 'unknown file type' do
       it 'includes a code fence with no language' do
         syntax_highlighter = Paperback::SyntaxHighlighter.new(
-          'user.xyz', 'abc123', nil
+          '', 'user.xyz', 'abc123', nil
         )
 
         actual = syntax_highlighter.to_ary
