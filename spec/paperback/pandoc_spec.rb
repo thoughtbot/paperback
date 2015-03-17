03aec1d9dee3d30f468833fc870e357f9e9ee9fa
patrick brisbin <pbrisbin@gmail.com>
2015-03-17 11:06:49 -0400

    Add spec coverage on Pandoc#generate

--- spec/paperback/pandoc_spec.rb
+++ spec/paperback/pandoc_spec.rb
@@ -1,16 +1,42 @@
 describe Paperback::Pandoc do
   describe "#generate" do
     before do
-      package = double(Paperback::Package).as_null_object
-      @pandoc = Paperback::Pandoc.new(package)
+      allow(Dir).to receive(:chdir).and_yield
+      allow(Paperback).to receive(:target_root).and_return("")
+
+      @package = double(Paperback::Package, target: "")
+      @pandoc = Paperback::Pandoc.new(@package)
     end
 
     context "with an unsupported version of pandoc" do
       it "raises an error" do
-        stub_command_line "pandoc 1.11.0"
+        stub_pandoc("--version", "pandoc 1.11.0")
 
         expect { @pandoc.generate(:foo, ["bar"]) }.to raise_error(RuntimeError)
       end
     end
+
+    it "adds --output=format to the given arguments" do
+      stub_pandoc("--version", "pandoc 1.11.1")
+      stub_pandoc("--foo --bar --output=html ", "")
+      allow(@package).to receive(:target).with("format").and_return("html")
+
+      @pandoc.generate("format", %w(--foo --bar))
+
+      expect_pandoc("--foo --bar --output=html ")
+    end
+  end
+
+  def stub_pandoc(arguments, output)
+    command_line = double(run: output)
+
+    allow(Cocaine::CommandLine).to receive(:new)
+      .with("pandoc", arguments)
+      .and_return(command_line)
+  end
+
+  def expect_pandoc(arguments)
+    expect(Cocaine::CommandLine).to have_received(:new)
+      .with("pandoc", arguments)
   end
 end
