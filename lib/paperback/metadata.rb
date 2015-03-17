thoughtbot/paperback

commit 3541866b7b0fd14eb57c7cfc288a87c15f521435

Fall back to `basename $PWD` for book name
This should allow us to have multiple books in one repository.

lib/paperback/metadata.rb
@@ -1,0 +1,12 @@
+module Paperback
+  class Metadata
+    def self.book_name
+      origin_url = Git.origin_url
+      if origin_url.empty?
+        File.basename(Dir.pwd)
+      else
+        origin_url
+      end
+    end
+  end
+end

spec/paperback/metadata_spec.rb
@@ -1,0 +1,20 @@
+describe Paperback::Metadata do
+  describe ".book_name" do
+    it "tries Git.origin_url first" do
+      expected = "http://example.com/foo/bar.git"
+      allow(Paperback::Git).to receive(:origin_url).and_return(expected)
+
+      actual = Paperback::Metadata.book_name
+
+      expect(actual).to eq(expected)
+    end
+
+    it "falls back to the current directory name" do
+      allow(Paperback::Git).to receive(:origin_url).and_return("")
+
+      actual = Paperback::Metadata.book_name
+
+      expect(actual).to eq("paperback")
+    end
+  end
+end

lib/paperback.rb
@@ -4,6 +4,7 @@ require "paperback/cover"
 require "paperback/formats"
 require "paperback/generators"
 require "paperback/git"
+require "paperback/metadata"
 require "paperback/hub"
 require "paperback/markdown"
 require "paperback/markdown_image_filter"
@@ -27,6 +28,6 @@ module Paperback
   end
 
   def self.target_root
-    build_root.join Git.repository_name
+    build_root.join Metadata.book_name
   end
 end

lib/paperback/git.rb
@@ -11,13 +11,7 @@ module Paperback
 
     def self.origin_url
       git = new("config --get remote.origin.url")
-      output = git.run.output.strip
-
-      if output.empty?
-        fail "No origin remote present."
-      end
-
-      output
+      git.run.output.strip
     end
 
     def self.repository_name

lib/paperback/package.rb
@@ -24,7 +24,7 @@ module Paperback
     def target(extension)
       Paperback
         .target_root
-        .join("#{Git.repository_name}#{suffix}")
+        .join("#{Metadata.book_name}#{suffix}")
         .sub_ext ".#{extension}"
     end
 

spec/paperback/git_spec.rb
@@ -11,10 +11,10 @@ describe Paperback::Git do
       expect(actual).to eq(expected)
     end
 
-    it "raises if there is no origin remote configured" do
+    it "is empty if there is no origin remote configured" do
       stub_command_line("\n")
 
-      expect { Paperback::Git.origin_url }.to raise_error(/no origin remote/i)
+      expect(Paperback::Git.origin_url).to eq ""
     end
   end
 

commit 38ebeef2a4197ea1f921434b206acd1f1ddd83a9

Have Metadata.book_name use Git.repository_name, not origin_url
- Fixes a bug introduced in 3541866b

lib/paperback/metadata.rb
@@ -1,11 +1,12 @@
 module Paperback
   class Metadata
     def self.book_name
-      origin_url = Git.origin_url
-      if origin_url.empty?
+      repository_name = Git.repository_name
+
+      if repository_name.empty?
         File.basename(Dir.pwd)
       else
-        origin_url
+        repository_name
       end
     end
   end

spec/paperback/metadata_spec.rb
@@ -1,8 +1,8 @@
 describe Paperback::Metadata do
   describe ".book_name" do
-    it "tries Git.origin_url first" do
-      expected = "http://example.com/foo/bar.git"
-      allow(Paperback::Git).to receive(:origin_url).and_return(expected)
+    it "tries Git.repository_name first" do
+      expected = "foo"
+      allow(Paperback::Git).to receive(:repository_name).and_return(expected)
 
       actual = Paperback::Metadata.book_name
 
@@ -10,7 +10,7 @@ describe Paperback::Metadata do
     end
 
     it "falls back to the current directory name" do
-      allow(Paperback::Git).to receive(:origin_url).and_return("")
+      allow(Paperback::Git).to receive(:repository_name).and_return("")
 
       actual = Paperback::Metadata.book_name
 