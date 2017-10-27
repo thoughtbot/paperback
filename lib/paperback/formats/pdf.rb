09e91e5a72dc08dffe121b4998c40c1a6f2da0ca
LkeMitchll <mail@luke.fail>
2017-10-27 16:40:34 +0100

    Add proper font version

--- lib/paperback/formats/pdf.rb
+++ lib/paperback/formats/pdf.rb
@@ -16,7 +16,7 @@ module Paperback
           "--pdf-engine=xelatex",
           "--template=#{pdf_latex_template_path}",
           "--toc",
-          '--variable=mainfont:"Tiempos Text Regular"',
+          '--variable=mainfont:"Tiempos Text"',
           "--variable=monofont:Inconsolata",
           '--variable=sansfont:"Calibre"',
           "--variable=links-as-notes:true"
