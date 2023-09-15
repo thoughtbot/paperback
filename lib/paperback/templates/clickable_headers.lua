a9b1938f6d48ac52950787e6e92400afad376fdc
Chad Pytel <chad@thoughtbot.com>
2023-09-15 15:15:20 -0400

    Don't link top level headers
    Currently, all headers made into links in the website build, but with
    the split-level set to 2, this means that all h1s and h2s, which are the
    first headers on the page are also made into links, which is awkward.
    
    This changes it so that only headers within the content (greater than
    level 2) are made into links.

--- lib/paperback/templates/clickable_headers.lua
+++ lib/paperback/templates/clickable_headers.lua
@@ -2,6 +2,8 @@ function Header (h)
   -- if AST object is header type, it is passed through this function
 
   -- update the content
-  h.content = pandoc.Link(h.content, '#' .. h.identifier)
+  if h.level > 2 then
+    h.content = pandoc.Link(h.content, '#' .. h.identifier)
+  end
   return h
 end
